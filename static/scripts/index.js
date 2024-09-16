563b2fcfc643479aa7eb7faf427a989aa7fcb72a
technicalmastersp <takebestservices@gmail.com>
2024-09-16 08:07:29 +0530

    home page all device responsiveness is done

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,12 +1,8 @@
 document.body.style.backgroundColor = "#ffcdef";
 
 const colorSection = document.getElementById("colorSection")
-// const colorSectionBox1 = document.getElementById("colorSectionBox1")
 const bgColorFill = document.getElementsByClassName("bgColorBox");
-// let bgColorCodeName = document.getElementsByClassName("bgColorCode");
 const colorFill = document.getElementsByClassName("colorBox");
-// let colorCodeName = document.getElementsByClassName("colorCode");
-
 
 function randomNo(){
     let val1 = Math.ceil(0 + Math.random() * 255);
@@ -16,22 +12,12 @@ function randomNo(){
 }
 
 Array.from(bgColorFill).forEach(e=>{
-    // e.style.backgroundColor = randomNo();
     let store = randomNo();
     e.style.backgroundColor = store;
-    let a = e.insertAdjacentHTML("afterend",`<p class="text-xs">BG Color : ${store}</p>`)
+    let a = e.insertAdjacentHTML("afterend",`<p class="text-xs lg:text-base">BG Color : ${store}</p>`)
 })
 Array.from(colorFill).forEach(e=>{
-    // e.style.backgroundColor = randomNo();
     let store = randomNo();
     e.style.backgroundColor = store;
     // let a = e.insertAdjacentHTML("afterend",`<p class="text-xs">Font Color : ${store}</p>`)
-
 })
-
-
-// for(let i=0; i<=100; i++){
-//     if(i % 2 == 0 && i % 3 == 0 && i % 4 == 0 && i % 5 == 0){
-//         console.log(i)
-//     }
-// }
